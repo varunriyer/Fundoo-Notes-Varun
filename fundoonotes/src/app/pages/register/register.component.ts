7d8e3b77cc4688f4271fcff87134a939ea821447
varunriyer <rvaruniyer@gmail.com>
2025-06-02 16:15:51 +0530

    feat: enhance registration component with improved layout, styling, and validation

--- fundoonotes/src/app/pages/register/register.component.ts
+++ fundoonotes/src/app/pages/register/register.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatCardModule } from '@angular/material/card';
 
 
 @Component({
@@ -17,35 +17,61 @@ import { MatCardModule } from '@angular/material/card';
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
-    MatCheckboxModule,
-    MatCardModule
+    MatCardModule,
+    MatCheckboxModule
   ],
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
   registerForm: FormGroup;
-  hidePassword = true;
 
   constructor(private fb: FormBuilder) {
-    this.registerForm = this.fb.group({
-      firstName: ['', [Validators.required]],
-      lastName: ['', [Validators.required]],
-      username: ['', [Validators.required, Validators.pattern(/^[a-zA-Z0-9.]+$/)]],
-      password: ['', [Validators.required, Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/)]],
-      confirmPassword: ['', [Validators.required]]
-    });
+    this.registerForm = this.fb.group(
+  {
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    username: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern(/^[a-zA-Z0-9.]+$/)
+      ]
+    ],
+    password: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/)
+      ]
+    ],
+    confirmPassword: ['', Validators.required]
+  },
+  { validators: this.passwordMatchValidator }
+);
+
   }
 
-  togglePasswordVisibility() {
-    this.hidePassword = !this.hidePassword;
-  }
+  passwordMatchValidator(form: FormGroup) {
+  const password = form.get('password')?.value;
+  const confirmPassword = form.get('confirmPassword')?.value;
 
-  onSubmit() {
-    if (this.registerForm.valid) {
-      console.log('Form Submitted:', this.registerForm.value);
-    } else {
-      this.registerForm.markAllAsTouched();
-    }
+  return password === confirmPassword ? null : { passwordMismatch: true };
+}
+hidePassword = true;
+
+togglePasswordVisibility() {
+  this.hidePassword = !this.hidePassword;
+}
+
+onSubmit() {
+  if (this.registerForm.valid) {
+    console.log('Form Submitted', this.registerForm.value);
+  } else {
+    this.registerForm.markAllAsTouched();
   }
 }
+
+}
+
+
