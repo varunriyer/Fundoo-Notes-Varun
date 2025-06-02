0b49f67676b07a8274b9b21bc38a65178d5491a5
varunriyer <rvaruniyer@gmail.com>
2025-06-02 14:27:32 +0530

    refactor: simplify app component by removing unused imports and updating template

--- fundoonotes/src/app/app.component.ts
+++ fundoonotes/src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RegisterComponent } from './pages/register/register.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [RegisterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'fundoonotes';
-}
+export class AppComponent {}
