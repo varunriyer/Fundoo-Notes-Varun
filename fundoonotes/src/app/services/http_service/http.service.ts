30a9cbeb97a4c8d5a831b2b98d720ef1e67e46de
varunriyer <rvaruniyer@gmail.com>
2025-06-03 14:56:43 +0530

    [Varun] Add:  HttpService with API methods and header management

---
+++ fundoonotes/src/app/services/http_service/http.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+   private baseUrl = 'https://fundoonotes.incubation.bridgelabz.com/api/';
+
+  constructor(private http: HttpClient) { }
+
+  getHeader(){
+    const header = new HttpHeaders({
+      Authorization : localStorage.getItem('token') || ''
+    })
+    return header;
+  }
+
+  getApi(endpoint:string, headers: HttpHeaders = new HttpHeaders()){
+    return this.http.get(this.baseUrl+ endpoint,{headers})
+  }
+
+   postApi(endpoint:string,payload:any, headers: HttpHeaders = new HttpHeaders()){
+    return this.http.post(this.baseUrl+ endpoint,payload,{headers})
+  }
+
+  putApi(endpoint:string,payload:any, headers: HttpHeaders = new HttpHeaders()){
+    return this.http.post(this.baseUrl+ endpoint,payload,{headers})
+  }
+  
+
+  
+}
