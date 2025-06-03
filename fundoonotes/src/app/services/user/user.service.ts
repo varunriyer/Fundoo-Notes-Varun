6e0d0742e11a13248e1c1dee2bb31b362f24604d
varunriyer <rvaruniyer@gmail.com>
2025-06-03 14:57:01 +0530

    [Varun] Add: UserService with login method for user authentication

---
+++ fundoonotes/src/app/services/user/user.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpService } from '../http_service/http.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http:HttpService) { }
+  
+  login(payload:any){
+    return this.http.postApi('user/login',payload)
+  }
+}
