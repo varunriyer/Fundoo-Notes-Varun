7614ab38057ee5af940c00eb624dd43d94b06c63
varunriyer <rvaruniyer@gmail.com>
2025-06-04 07:38:20 +0530

    [Varun] Add: User service to post the data to the API

--- fundoonotes/src/app/pages/register/register.component.ts
+++ fundoonotes/src/app/pages/register/register.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  ReactiveFormsModule,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-
+import { UserService } from 'src/app/services/user/user.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -18,60 +24,69 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
     MatInputModule,
     MatButtonModule,
     MatCardModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    RouterModule,
   ],
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
   registerForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private user: UserService) {
     this.registerForm = this.fb.group(
-  {
-    firstName: ['', Validators.required],
-    lastName: ['', Validators.required],
-    username: [
-      '',
-      [
-        Validators.required,
-        Validators.pattern(/^[a-zA-Z0-9.]+$/)
-      ]
-    ],
-    password: [
-      '',
-      [
-        Validators.required,
-        Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/)
-      ]
-    ],
-    confirmPassword: ['', Validators.required]
-  },
-  { validators: this.passwordMatchValidator }
-);
-
+      {
+        firstName: ['', Validators.required],
+        lastName: ['', Validators.required],
+        username: ['', Validators.pattern(/^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/)],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/),
+          ],
+        ],
+        confirmPassword: ['', Validators.required],
+      },
+      { validators: this.passwordMatchValidator }
+    );
   }
 
   passwordMatchValidator(form: FormGroup) {
-  const password = form.get('password')?.value;
-  const confirmPassword = form.get('confirmPassword')?.value;
+    const password = form.get('password')?.value;
+    const confirmPassword = form.get('confirmPassword')?.value;
 
-  return password === confirmPassword ? null : { passwordMismatch: true };
-}
-hidePassword = true;
+    return password === confirmPassword ? null : { passwordMismatch: true };
+  }
+  hidePassword = true;
 
-togglePasswordVisibility() {
-  this.hidePassword = !this.hidePassword;
-}
+  togglePasswordVisibility() {
+    this.hidePassword = !this.hidePassword;
+  }
 
-onSubmit() {
-  if (this.registerForm.valid) {
-    console.log('Form Submitted', this.registerForm.value);
-  } else {
-    this.registerForm.markAllAsTouched();
+  onSubmit() {
+    if (this.registerForm.valid) {
+      const { firstName, lastName, username, password } =
+        this.registerForm.value;
+
+      const data = {
+        firstName: firstName,
+        lastName: lastName,
+        email: username, // need to confirm this
+        password: password,
+        service: 'advance',
+      };
+
+      this.user.register(data).subscribe({
+        next: (res: any) => {
+          console.log('Registered successfully', res);
+        },
+        error: (err) => {
+          console.error('Registration failed', err);
+        },
+      });
+    } else {
+      this.registerForm.markAllAsTouched();
+    }
   }
 }
-
-}
-
-
