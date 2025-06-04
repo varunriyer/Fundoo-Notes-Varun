bd33fe76fed441c542930b696dff9ae286e20d71
varunriyer <rvaruniyer@gmail.com>
2025-06-04 15:37:01 +0530

    [Varun] Fix: Correct import path for DashboardComponent in app.routes.ts

--- fundoonotes/src/app/app.routes.ts
+++ fundoonotes/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
-import { DashboardComponent } from './pages/dashboard/dashboard/dashboard.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { AuthGuardService } from './services/authguard.service';
 
 export const routes: Routes = [
