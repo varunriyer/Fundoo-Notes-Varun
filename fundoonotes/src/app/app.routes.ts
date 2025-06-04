7585620af0d0e54d0a21962d2458bfd522c34737
varunriyer <rvaruniyer@gmail.com>
2025-06-04 14:25:33 +0530

    [Varun] Refactor: add dashboard route with authentication guard

--- fundoonotes/src/app/app.routes.ts
+++ fundoonotes/src/app/app.routes.ts
@@ -1,9 +1,17 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { DashboardComponent } from './pages/dashboard/dashboard/dashboard.component';
+import { AuthGuardService } from './services/authguard.service';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [AuthGuardService],
+  },
 ];
