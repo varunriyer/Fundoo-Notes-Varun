9a55c61ca5fc1ead3a6cf8408971738121dd85a7
varunriyer <rvaruniyer@gmail.com>
2025-06-05 11:32:40 +0530

    [Varun] Add: Implement dashboard layout with sidenav and toolbar components

--- fundoonotes/src/app/pages/dashboard/dashboard.component.ts
+++ fundoonotes/src/app/pages/dashboard/dashboard.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToolbarComponent } from './toolbar/toolbar.component';
-import { SidenavComponent } from './sidenav/sidenav.component';
+import { SidenavComponent } from './toolbar/sidenav/sidenav.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, ToolbarComponent, SidenavComponent],
+  imports: [CommonModule, ToolbarComponent, SidenavComponent, MatSidenavModule],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  isSidenavOpen = false;
+  isHovered = false;
+
+  get sidenavOpened(): boolean {
+    return this.isSidenavOpen || this.isHovered;
+  }
+
+  onMouseEnterSidenav() {
+    this.isHovered = true;
+  }
+
+  onMouseLeaveSidenav() {
+    this.isHovered = false;
+  }
+}
