35b7fb8be422d78182b0df41d29c20a3f94b974a
varunriyer <rvaruniyer@gmail.com>
2025-06-05 17:11:49 +0530

    [Varun] Add: Implement NotesService with methods to add and retrieve notes

---
+++ fundoonotes/src/app/services/notes/notes.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpService } from '../http_service/http.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NotesService {
+  constructor(private http: HttpService) {}
+
+  addNote(payload: any) {
+    const headers = this.http.getHeader();
+    return this.http.postApi('notes/addNotes', payload, headers);
+  }
+
+  getAllNotes() {
+    const headers = this.http.getHeader();
+    return this.http.getApi('notes/getNotesList', headers);
+  }
+}
