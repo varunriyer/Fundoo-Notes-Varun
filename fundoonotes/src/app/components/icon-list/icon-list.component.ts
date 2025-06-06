fbaf600ba2463d31beaea1670389580121ccd7ea
varunriyer <rvaruniyer@gmail.com>
2025-06-06 08:20:07 +0530

    [Varun] Add: Create IconListComponent with template, styles, and tests

---
+++ fundoonotes/src/app/components/icon-list/icon-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-icon-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './icon-list.component.html',
+  styleUrls: ['./icon-list.component.css']
+})
+export class IconListComponent {
+
+}
