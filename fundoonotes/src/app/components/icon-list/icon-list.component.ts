3d351d9b6b079a807f726a6aae7c66a7f06532c9
varunriyer <rvaruniyer@gmail.com>
2025-06-07 06:16:11 +0530

    [Varun] Add: Implement IconListComponent with styles, template, and input handling

--- fundoonotes/src/app/components/icon-list/icon-list.component.ts
+++ fundoonotes/src/app/components/icon-list/icon-list.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-icon-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './icon-list.component.html',
-  styleUrls: ['./icon-list.component.css']
+  styleUrls: ['./icon-list.component.css'],
 })
 export class IconListComponent {
-
+  @Input() context: 'input' | 'card' = 'input';
 }
