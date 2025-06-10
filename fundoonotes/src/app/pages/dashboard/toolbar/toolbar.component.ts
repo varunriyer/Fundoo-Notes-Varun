e008c85b3b8ab95217800e609a95b65c6f333540
varunriyer <rvaruniyer@gmail.com>
2025-06-10 09:26:45 +0530

    [Varun] Add: Implement dynamic title update in toolbar based on current route

--- fundoonotes/src/app/pages/dashboard/toolbar/toolbar.component.ts
+++ fundoonotes/src/app/pages/dashboard/toolbar/toolbar.component.ts
@@ -1,8 +1,11 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+
 @Component({
   selector: 'app-toolbar',
   standalone: true,
@@ -10,8 +13,39 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.css'],
 })
-export class ToolbarComponent {
+export class ToolbarComponent implements OnInit {
   isGrid = true;
+  mobileSearchOpen = false;
+
+  @Output() menuClicked = new EventEmitter<void>();
+
+  currentTitle: string = 'Keep';
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        const routeSegment = this.router.url.split('/').pop();
+        switch (routeSegment) {
+          case 'notes':
+            this.currentTitle = 'Keep';
+            break;
+          case 'reminders':
+            this.currentTitle = 'Reminders';
+            break;
+          case 'archive':
+            this.currentTitle = 'Archive';
+            break;
+          case 'trash':
+            this.currentTitle = 'Trash';
+            break;
+          default:
+            this.currentTitle = 'Keep';
+        }
+      });
+  }
 
   toggleView() {
     this.isGrid = !this.isGrid;
@@ -21,11 +55,7 @@ export class ToolbarComponent {
     location.reload();
   }
 
-  @Output() menuClicked = new EventEmitter<void>();
-
   toggleSidebar() {
     this.menuClicked.emit();
   }
-
-  mobileSearchOpen = false;
 }
