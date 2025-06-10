25daa7868723fa3fc158fdc9ec094fabc31f43ac
varunriyer <rvaruniyer@gmail.com>
2025-06-10 08:43:30 +0530

    [Varun] Add: Implement color palette feature in icon list with color selection and toggle functionality

--- fundoonotes/src/app/components/icon-list/icon-list.component.ts
+++ fundoonotes/src/app/components/icon-list/icon-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 
@@ -11,4 +11,31 @@ import { MatIconModule } from '@angular/material/icon';
 })
 export class IconListComponent {
   @Input() context: 'input' | 'card' = 'input';
+  @Output() colorSelected = new EventEmitter<string>();
+
+  showColorPalette = false;
+
+  lightColors: string[] = [
+    '#ffffff',
+    '#f28b82',
+    '#fbbc04',
+    '#fff475',
+    '#ccff90',
+    '#a7ffeb',
+    '#cbf0f8',
+    '#aecbfa',
+    '#d7aefb',
+    '#fdcfe8',
+    '#e6c9a8',
+    '#e8eaed',
+  ];
+
+  toggleColorPalette() {
+    this.showColorPalette = !this.showColorPalette;
+  }
+
+  selectColor(color: string) {
+    this.colorSelected.emit(color);
+    this.showColorPalette = false;
+  }
 }
