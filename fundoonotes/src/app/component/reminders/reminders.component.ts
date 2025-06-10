70882541fe7684dad7624f9b5c689e0563674f80
varunriyer <rvaruniyer@gmail.com>
2025-06-10 09:32:56 +0530

    [Varun] Add: Implement Reminders component with HTML, CSS, and unit tests

---
+++ fundoonotes/src/app/component/reminders/reminders.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-reminders',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './reminders.component.html',
+  styleUrls: ['./reminders.component.css']
+})
+export class RemindersComponent {
+
+}
