dd1eb16552662ab99e1c63a18656621e697f088a
varunriyer <rvaruniyer@gmail.com>
2025-06-10 09:51:26 +0530

    [Varun] Add: Implement Reminders component with layout and styles

--- fundoonotes/src/app/components/reminders/reminders.component.ts
+++ fundoonotes/src/app/components/reminders/reminders.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-reminders',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './reminders.component.html',
-  styleUrls: ['./reminders.component.css']
+  styleUrls: ['./reminders.component.css'],
 })
-export class RemindersComponent {
-
-}
+export class RemindersComponent {}
